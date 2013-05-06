62a3edb83751df1e7ef14445ee40cb459b6d3892
Tien Nguyen <nqtien310@gmail.com>
2013-05-07 03:39:14 +0700

    Add install generator

---
+++ lib/generators/quick_shoulda/install/install_generator.rb
@@ -0,0 +1,11 @@
+module QuickShoulda
+  class InstallGenerator < ::Rails::Generators::Base
+    def self.source_root
+	    @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+	  end
+
+	  def copy_config_file
+      template '.qshoulda.yml'
+    end
+  end	
+end
